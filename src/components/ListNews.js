8943de43819b4d4f8c861d31f79f9240e33fb5a2
RoswellCR <rosbelcr@gmail.com>
2022-08-26 18:49:46 -0400

    doing the main functions

--- src/components/ListNews.js
+++ src/components/ListNews.js
@@ -1,8 +1,20 @@
+import React, {useContext, useEffect} from 'react';
+import NewsContext from '../contexts/newsContext';
 import News from "./News";
 
 export default function ListNews(){
+    const {listAng, getListNews} = useContext(NewsContext);
     
+    useEffect(()=>{
+         getListNews();
+    },[])
     
+    // useEffect(()=>{
+    //     fetch('https://hn.algolia.com/api/v1/search_by_date?query=angular&page=0')
+    //     .then(res=>res.json())
+    //     .then(datos=>console.log(datos.hits))
+    // },[])
+
     return(
         <div>
             <div><h1>Hacker News</h1></div>
@@ -19,7 +31,7 @@ export default function ListNews(){
                 </select>
             </div>
             <div>
-                Lista de <News />
+                 {listAng.map((story)=><div key={story.story_id}><News story={story} /></div> ) } 
             </div>
             <div>1 2 3 4 ...</div>
         </div>
